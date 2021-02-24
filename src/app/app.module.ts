9dfe715459e33214b8d1c8b01d6449308cfd7f51
git <abonini2000@gmail.com>
2021-02-23 22:14:44 -0300

    initial code in home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/containers/home/home.component';
 
@@ -12,7 +12,10 @@ import { HomeComponent } from './home/containers/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'home', component: HomeComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/containers/home/home.component.ts
+++ src/app/home/containers/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  title: string;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  updateTitle(value) {
+    console.log(`updateTitle: ${value}`);
+    this.title = value;
   }
-
 }
