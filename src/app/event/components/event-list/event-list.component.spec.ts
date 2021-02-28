7b0f03c3d4c177eb1e20908179f58f039d2dc5c5
git <abonini2000@gmail.com>
2021-02-28 01:44:47 -0300

    testing list

--- src/app/event/components/event-list/event-list.component.spec.ts
+++ src/app/event/components/event-list/event-list.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Attendee } from './../../../models';
 
 import { EventListComponent } from './event-list.component';
 
@@ -6,12 +7,12 @@ describe('EventListComponent', () => {
   let component: EventListComponent;
   let fixture: ComponentFixture<EventListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ EventListComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EventListComponent);
@@ -22,4 +23,19 @@ describe('EventListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have no attendees on load', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have 1 attendee on load', () => {
+    component.attendees = [
+      {name: 'Duncan', attending: true, guests: 2}
+    ] as Attendee[];
+  
+    fixture.detectChanges();
+
+    expect(component).toMatchSnapshot();
+  });
 });
+
