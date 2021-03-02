34a69b6313c97dd8de1ec9f061a4a36c03e97fbe
git <abonini2000@gmail.com>
2021-03-01 21:52:27 -0300

    finishing ui components logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AddCardComponent } from './ui/add-card/add-card.component';
 import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { ErrorComponent } from './ui/error/error.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
     HomeComponent,
     AddCardComponent,
     WeatherCardComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
